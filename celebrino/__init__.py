324c6c819304fe447b923caa89994585b1fda7f7
hkrhemendra <hemendralalawat30@gmail.com>
2022-09-24 22:11:14 +0530

    Changes

--- celebrino/__init__.py
+++ celebrino/__init__.py
@@ -6,14 +6,14 @@ from itsdangerous import URLSafeTimedSerializer
 import os
 import sshtunnel
 
-sshtunnel.SSH_TIMEOUT = 5.0
-sshtunnel.TUNNEL_TIMEOUT = 5.0
+# sshtunnel.SSH_TIMEOUT = 5.0
+# sshtunnel.TUNNEL_TIMEOUT = 5.0
 
-tunnel = sshtunnel.SSHTunnelForwarder(
-    ('ssh.pythonanywhere.com'), ssh_username='hemendra123',ssh_password = 'Hemendra@123',
-    remote_bind_address=('hemendra123.mysql.pythonanywhere-services.com', 3306)
+# tunnel = sshtunnel.SSHTunnelForwarder(
+#     ('ssh.pythonanywhere.com'), ssh_username='hemendra123',ssh_password = 'Hemendra@123',
+#     remote_bind_address=('hemendra123.mysql.pythonanywhere-services.com', 3306)
 
-)
+# )
 
 #Flask app
 app = Flask(__name__)
@@ -26,13 +26,13 @@ ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 ALLOWED_EXTENSIONS_GALLERY = {'jpg','jpeg','mp4'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-tunnel.start()
+# tunnel.start()
 
 #Tokenization
 s = URLSafeTimedSerializer("Thisismyscrete")
 
 #Database config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hemendra123:celebrino123@hemendra123.mysql.pythonanywhere-services.com/hemendra123$default'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql6521945:VyPeZUxFa1@sql6.freemysqlhosting.net:3306/sql6521945'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
